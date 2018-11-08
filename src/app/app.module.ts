a3c1ae5a1f6bd8e591d7794d3ca3e4340b90323f
maxdhn <maxduchan@gmail.com>
2018-11-08 10:34:26 +0100

    new component 'activity-list'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActivityFormComponent } from './activity-form/activity-form.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ActivityFormComponent
+    ActivityFormComponent,
+    ActivityListComponent
   ],
   imports: [
     BrowserModule,
